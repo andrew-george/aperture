da3a3f38217c5acd7674e2e8644dc8a280584da6
Andrew George <andrewgeorgeberty@gmail.com>
2023-05-10 04:32:11 +0300

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
+import 'styles/globals.css'
+import Layout from '../Components/layout/Layout'
+import { store } from '../store/store'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<Provider store={store}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</Provider>
+	)
 }
